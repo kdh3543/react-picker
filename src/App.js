89d1611b140f466384295baaadec858fc3296ddd
김동현 <rlaehdgus3543@naver.com>
2023-07-28 21:15:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import './App.css'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 function App() {
+  let [start, setStart] = useState(new Date())
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="calendar-box">
+        <DatePicker selected={start} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
